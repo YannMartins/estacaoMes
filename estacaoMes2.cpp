c580135fb700777ba5c60264a14a225678bdae2f
Yann Martins <102706324+YannMartins@users.noreply.github.com>
2022-04-02 21:20:15 -0300

    Update estacaoMes2.cpp

--- estacaoMes2.cpp
+++ estacaoMes2.cpp
@@ -1,11 +1,3 @@
-/*
-=========================================================
-Name        : estacaoMes2
-Author      : Yann Martins
-Description : Program in C++
-=========================================================
-*/
-
 #include <stdio.h>
 int main ()
 {
@@ -32,4 +24,3 @@ int main ()
     getchar();
     return 0;
 }
-

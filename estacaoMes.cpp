0e05c74af66b341a5fea5fae0c0b53f445e5a491
Yann Martins <102706324+YannMartins@users.noreply.github.com>
2022-04-06 20:58:59 -0300

    C and C++

--- estacaoMes.cpp
+++ estacaoMes.cpp
@@ -1,23 +1,21 @@
 #include <stdio.h>
-int main ()
-{
+
+int main(void){
     int mes;
+    
     printf("Identifica a estacao do ano de acordo com o mes correspondente.\n");
     printf("Informe o numero do mes: ");
     scanf(" %d",&mes);
-    if(mes>=1 &mes<=3){
+    
+    if (mes>=1 &mes<=3){
         printf("\nVerao.\n");
-    }
-    else if(mes>=4 &mes<=6){
+    }else if (mes>=4 &mes<=6){
         printf("\nOutono.\n");
-    }
-    else if(mes>=7 &mes<=9){
+    }else if (mes>=7 &mes<=9){
         printf("\nInverno.\n");
-    }    
-    else if(mes>=10 &mes<=12){
+    }else if (mes>=10 &mes<=12){
         printf("\nPrimavera.\n");
-    }    
-    else{
+    }else{
         printf("\nMes informado errado.\n");
     }
     
